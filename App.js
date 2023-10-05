a8f1bfffd57aaf246d557a54823a5576d0ce4776
SonKimDev <kimson2507.contact@gmail.com>
2023-10-05 20:02:40 +0700

    finish commit

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native'
 import React from 'react'
+import WebView from 'react-native-webview'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <WebView
+          source={{ uri: 'https://www.google.com.vn/' }} // Đặt URL bạn muốn hiển thị
+          style={{ flex: 1 }}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
